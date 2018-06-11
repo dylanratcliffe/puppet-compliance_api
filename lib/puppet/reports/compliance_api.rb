e6e7079e63c8e2b4e8738a02eecb1cfc12ff0cb9
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-06-11 17:17:50 +1000

    Initial commit

---
+++ lib/puppet/reports/compliance_api.rb
@@ -0,0 +1,21 @@
+require 'json'
+require 'puppet'
+require 'net/http'
+
+Puppet::Reports.register_report(:compliance_api) do
+  desc "Sends reports to compliance_api"
+
+  configfile = File.join([File.dirname(Puppet.settings[:config]), "compliance_api.yaml"])
+  raise(Puppet::ParseError, "compliance_api report config file #{configfile} not readable") unless File.exist?(configfile)
+  config = YAML.load_file(configfile)
+  uri    = URI.new(config['uri'])
+  CONN   = Net::HTTP.new(uri)
+  QUEUE  = config['queue'] || 'reports'
+
+  def process
+    # TODO: Use certificate auth
+
+    CONN.post("/q/#{QUEUE}", JSON.generate(self))
+
+  end
+end
