c7dce7cbf92fdf42499c731c1a604655730d19a9
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-06-11 19:56:31 +1000

    test request

--- lib/puppet/reports/compliance_api.rb
+++ lib/puppet/reports/compliance_api.rb
@@ -1,20 +1,24 @@
 require 'json'
 require 'puppet'
 require 'net/http'
+require 'uri'
 
 Puppet::Reports.register_report(:compliance_api) do
   desc "Sends reports to compliance_api"
 
-  configfile = File.join([File.dirname(Puppet.settings[:config]), "compliance_api.yaml"])
+  configfile   = File.join([File.dirname(Puppet.settings[:config]), "compliance_api.yaml"])
   raise(Puppet::ParseError, "compliance_api report config file #{configfile} not readable") unless File.exist?(configfile)
   config       = YAML.load_file(configfile)
   queue        = config['queue'] || 'reports'
-  REPORT_QUEUE = URI("#{config['uri']}/q/#{queue}")
+  uri          = URI("#{config['uri']}/q/#{queue}")
+  HTTP         = Net::HTTP.new(uri.host, uri.port)
+  REQUEST      = Net::HTTP::Post.new(uri.request_uri, header)
 
   def process
     # TODO: Use certificate auth
 
-    Net::HTTP.post(REPORT_QUEUE, "value=#{self.to_json}")
+    REQUEST.body = user.to_json
+    HTTP.request(REQUEST)
 
   end
 end
